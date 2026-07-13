992ab7dd2db549dd36a39af0ef34f992f7b87b79
Hazel Varshney <hazelvarshney93@gmail.com>
2026-07-13 12:16:22 +0530

    Create CODE.py

---
+++ CODE.py
@@ -0,0 +1,79 @@
+import cv2 as cv
+import numpy as np
+import time
+
+vid = cv.VideoCapture("ferrari.mp4")
+
+fps = vid.get(cv.CAP_PROP_FPS)
+frame_time = 1000 / fps
+lower_red1 = np.array([0, 80, 40])
+upper_red1 = np.array([10, 255, 255])
+lower_red2 = np.array([170, 80, 40])
+upper_red2 = np.array([180, 255, 255])
+
+hsv_times = []
+mask_times = []
+contour_times = []
+draw_times = []
+
+frame_count = 0
+
+trail_points = []
+
+
+while True:
+    start = time.time()
+
+    ret, frame = vid.read()
+
+    if not ret:
+        break
+    t1 = time.time()
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv_times.append((time.time() - t1)*1000)
+
+    t2  = time.time()
+    mask1 = cv.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv.inRange(hsv, lower_red2, upper_red2)
+    red_mask = cv.bitwise_or(mask1,mask2)
+    mask_times.append((time.time() - t2)*1000)
+
+    t3 = time.time()
+    contours, _ = cv.findContours(red_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    contour_times.append((time.time() - t3)*1000)
+
+    t4 = time.time()
+
+    if contours:
+        largest = max(contours, key=cv.contourArea)
+        area = cv.contourArea(largest)
+        if area > 500:
+            x, y, w, h = cv.boundingRect(largest)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    draw_times.append((time.time() - t4)*1000)
+
+    if area>500:
+        x,y,w,h = cv.boundingRect(largest)
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        center = (x+w//2, y+h//2)
+        trail_points.append(center)
+    for i in range(1, len(trail_points)):
+        cv.line(frame, trail_points[i - 1], trail_points[i], (255, 0, 0), 2)
+    cv.imshow("Tracking", frame)
+    cv.imshow("Mask", red_mask)
+
+    elapsed = (time.time() - start) * 1000
+    remaining_delay = max(1, int(frame_time - elapsed))
+
+    if cv.waitKey(remaining_delay) & 0xFF == ord('q'):
+        break
+
+vid.release()
+cv.destroyAllWindows()
+
+print(f"\nProcessed {frame_count} frames\n")
+print(f"{'Stage':25s}{'Avg Time (ms)':>15s}")
+print(f"{'Color Conversion (HSV)':25s}{np.mean(hsv_times):>15.3f}")
+print(f"{'Masking (inRange+OR)':25s}{np.mean(mask_times):>15.3f}")
+print(f"{'Contour Detection':25s}{np.mean(contour_times):>15.3f}")
+print(f"{'Draw Bounding Box':25s}{np.mean(draw_times):>15.3f}")
